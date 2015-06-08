c26025d272fe8caf86c7082708f0db23a78a5562
Mark Finger <markfinger@gmail.com>
2015-06-08 15:30:55 +1000

    Cleanup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import build from './build';
 import hmr from './hmr';
 import env from './env';
-import * as options from './options';
 
 build.hmr = hmr;
 build.env = env;
-build.options = options;
 
 export default build;
