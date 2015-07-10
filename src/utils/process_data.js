19d30c1272c4a8b21e8c883801d8c9d35383d629
Mark Finger <markfinger@gmail.com>
2015-07-10 12:36:56 +1000

    Ansi sequences are now stripped from error messages.
    This assists with producing human-readable output.

--- src/utils/process_data.js
+++ src/utils/process_data.js
@@ -1,13 +1,35 @@
+import stripAnsi from 'strip-ansi';
+import _ from 'lodash';
+
+const cleanError = (err) => {
+  // Strips ansi sequences from the error's strings
+
+  if (_.isString(err)) {
+    return stripAnsi(err);
+  }
+
+  let error = {
+    type: err.constructor.name,
+    message: stripAnsi(err.message)
+  };
+
+  if (err.stack) {
+    error.stack = stripAnsi(err.stack);
+  }
+
+  return error;
+};
+
 const processData = (err, data) => {
   // Processes data for communication between processes
 
   let error = null;
-  if (err && err.message) {
-    error = {
-      type: err.constructor.name,
-      message: err.message,
-      stack: err.stack
-    }
+  if (err) {
+    error = cleanError(err);
+  }
+
+  if (data && data.stats && data.stats.errors) {
+    data.stats.errors = data.stats.errors.map(cleanError);
   }
 
   return {
